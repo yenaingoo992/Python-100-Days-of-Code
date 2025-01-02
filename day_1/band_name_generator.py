025b45474d7901e574097b27db74dceeff2f7e1a
Ye Naing Oo <yenaingoo992@gmail.com>
2025-01-02 09:53:35 +0630

    Day 1 of 100DaysOfChllange

---
+++ day_1/band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator....!\n")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_or_color_name = input("What's your pet's name or your favorite color?\n")
+band_name = city_name + pet_or_color_name
+print("Your band name could be", band_name)
---
+++ day_1/main.py
@@ -0,0 +1,13 @@
+# printing on the console
+print("Hello World")
+
+# getting input on the console
+print("Hello " + input("What is your name?") + "!")
+
+# using variable to receive input
+name = input("Which football team you like most?")
+print("Favourite team: " + name)
+
+# len() is used to calculate string count
+count = len(name)
+print("Team name length count: ", count)
