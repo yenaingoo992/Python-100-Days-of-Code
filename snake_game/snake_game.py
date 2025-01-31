6ccef35233c25c76b858002db737cb6c500cc257
Ye Naing Oo <yenaingoo992@gmail.com>
2025-01-31 23:58:22 +0630

    Day 24 of 100DaysOfCode
    - learned file read, write
    - update snake game high score record using file

--- snake_game/snake_game.py
+++ snake_game/snake_game.py
@@ -31,17 +31,16 @@ while is_game_on:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
-        scoreboard.refresh()
 
     # detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        is_game_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.restart()
 
     # detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            is_game_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.restart()
 
 screen.exitonclick()
